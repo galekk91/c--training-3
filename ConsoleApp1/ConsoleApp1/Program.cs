e505027661abf8c76cdbc5b09f1c74f06c67bdea
Dawid Gal <d.gal@poczta.onet.pl>
2020-06-21 09:57:18 +0200

    Removed previous steps, added fibonnaci code

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,38 +7,15 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var names = new List<string> { "Dawid", "Ana", "Felipe" };
-            foreach (var name in names)
-            {
-                Console.WriteLine($"Hello {name.ToUpper()}!");
-            }
+            var fibonacciNumbers = new List<int> { 1, 1 };
 
-            Console.WriteLine();
-            names.Add("Maria");
-            names.Add("Bill");
-            names.Remove("Ana");
-            foreach (var name in names)
-            {
-                Console.WriteLine($"Hello {name.ToUpper()}!");
-            }
+            var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+            var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
 
-            Console.WriteLine($"My name is {names[0]}.");
-            Console.WriteLine($"I've added {names[2]} and {names[3]} to the list.");
+            fibonacciNumbers.Add(previous + previous2);
 
-            Console.WriteLine($"The list has {names.Count} people in it");
-
-            var index = names.IndexOf("Felipe");
-            if (index != -1)
-                Console.WriteLine($"The name {names[index]} is at index {index}");
-
-            var notFound = names.IndexOf("Not Found");
-            Console.WriteLine($"When an item is not found, IndexOf returns {notFound}");
-
-            names.Sort();
-            foreach (var name in names)
-            {
-                Console.WriteLine($"Hello {name.ToUpper()}!");
-            }
+            foreach (var item in fibonacciNumbers)
+                Console.WriteLine(item);
         }
     }
 }
