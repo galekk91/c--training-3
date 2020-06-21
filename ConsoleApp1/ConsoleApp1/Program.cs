1db3909ba192ef6e8326138551b533915857c1a6
Dawid Gal <d.gal@poczta.onet.pl>
2020-06-21 09:52:06 +0200

    Removed one name, added two more

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,6 +12,14 @@ namespace ConsoleApp1
             {
                 Console.WriteLine($"Hello {name.ToUpper()}!");
             }
+            Console.WriteLine();
+            names.Add("Maria");
+            names.Add("Bill");
+            names.Remove("Ana");
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
         }
     }
 }
